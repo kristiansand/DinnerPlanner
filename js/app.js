a78fc13a9df93e4de72a96378412666fec084837
Martin Othamar <martin@othamar.net>
2014-09-24 21:34:33 +0200

    AngularJS implemented, map fixed

---
+++ js/app.js
@@ -0,0 +1,19 @@
+angular
+  .module('app', [
+    'ui.router',
+    'google-maps'
+  ])
+  .config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider) {
+    $urlRouterProvider.otherwise('/');
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'home.html',
+        controller: 'homeCtrl',
+      })
+      .state('about', {
+        url: '/about',
+        templateUrl: 'about.html',
+        controller: 'aboutCtrl'
+      })
+  }])
