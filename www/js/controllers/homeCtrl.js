6f452fa2990683c648992a20f9e6d8828164cb17
Martin Othamar <martin@othamar.net>
2014-09-25 13:33:20 +0200

    Remove maps

--- www/js/controllers/homeCtrl.js
+++ www/js/controllers/homeCtrl.js
@@ -9,11 +9,4 @@ angular
   .module('app')
   .controller('homeCtrl', ['$scope', function($scope) {
     $scope.title = "Home";
-    $scope.map = {
-        center: {
-            latitude: 58.147406,
-            longitude: 7.996714
-        },
-        zoom: 15
-    };
   }]);
