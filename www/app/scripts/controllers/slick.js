d1d8d348fbac552e96f15f918ecad99dd8f8ee46
Martin Othamar <martin@othamar.net>
2014-10-25 14:33:08 +0200

    slickctrl

--- www/app/scripts/controllers/slick.js
+++ www/app/scripts/controllers/slick.js
@@ -2,12 +2,12 @@
 
 /**
  * @ngdoc function
- * @name angularjsApp.controller:MainCtrl
+ * @name DinnerPlanner.controller:SlickCtrl
  * @description
- * # MainCtrl
- * Controller of the angularjsApp
+ * # SlickCtrl
+ * Controller of the DinnerPlanner
  */
-angular.module('angularjsApp')
+angular.module('DinnerPlanner')
   .controller('SlickCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
